259584d02842feb6ad9801b7546fdb28c3449893
csteve18 <csteve18@u.rochester.edu>
2017-06-21 10:21:18 -0400

    Update default.js

--- default.js
+++ default.js
@@ -1,16 +1,1 @@
-  $(document).ready(function (){
-	var stickyNavTop = $('.navbar').offset().top;
-	var stickyNav = function(){
-		var scrollTop = $(window).scrollTop();
-		if(scrollTop > stickyNavTop){
-			$('.navbar').addClass('sticky');
-		}
-		else {
-			$('.nav').removeClass('sticky');
-		}
-	};
-	stickyNav();
-	$(window).scroll(function(){
-		stickyNav();
-	});	  
-  });
+
